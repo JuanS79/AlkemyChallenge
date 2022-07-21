7ebe338e850ecb9cb0096e95e20c0b17a789039c
JuanS79 <42647173+JuanS79@users.noreply.github.com>
2022-07-21 20:16:19 -0300

    Add files via upload

--- sql/tabla.sql
+++ sql/tabla.sql
@@ -1,1 +1,14 @@
-
+CREATE TABLE IF NOT EXISTS tabla_prueba(
+            ext_date date PRIMARY KEY,
+            id_provincia Integer NOT NULL,
+            id_departamento Integer NOT NULL,
+            categoria VARCHAR(255) NOT NULL,
+            provincia VARCHAR(255) NOT NULL,
+            localidad VARCHAR(255) NOT NULL,
+            nombre VARCHAR (255) NOT NULL,
+            domicilio VARCHAR (255) NOT NULL,
+            codigo_postal VARCHAR (255) NOT NULL,
+            número_teléfono VARCHAR (255) NOT NULL,
+            mail VARCHAR(255) NOT NULL,
+            web VARCHAR (255) NOT NULL
+            );
